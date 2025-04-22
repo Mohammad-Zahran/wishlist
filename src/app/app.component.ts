e2b962e17c9439619eba7616369a59d4206b3bc1
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-04-22 12:33:57 +0300

    Added the FormsModule inside the app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common'; // ✅ Import this
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Import FormsModule for ngModel
 import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true, // ✅ If you're using standalone components
-  imports: [RouterOutlet, CommonModule], // ✅ Add CommonModule here
+  imports: [RouterOutlet, CommonModule, FormsModule], // ✅ Add CommonModule here
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
@@ -16,7 +17,15 @@ export class AppComponent {
     new WishItem('Get Coffee', true),
     new WishItem('Find grass that cuts itself'),
   ];
+
+  newWishText = '';
+
   title = 'wishlist';
+
+  addNewWish() {
+    // todo: add wish to items array
+    // clear the textbox
+  }
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
