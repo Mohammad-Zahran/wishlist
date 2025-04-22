2d6cdade15ca728efc52a493026b554075d51115
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-04-22 13:13:05 +0300

    Created the filter function inside the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,16 +12,34 @@ import { WishItem } from '../shared/models/wishItem';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  items: WishItem[] = [];
+  items: WishItem[] = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself'),
+  ];
+
+  listFilter: String = '0';
 
   newWishText = '';
 
   title = 'wishlist';
 
+  visibleItems: WishItem[] = this.items;
+
   addNewWish() {
     if (this.newWishText.trim()) {
       this.items.push(new WishItem(this.newWishText.trim()));
       this.newWishText = '';
+    }
+  }
+
+  filterChanged(value: any) {
+    if (value === '0') {
+      this.visibleItems = this.items;
+    } else if (value === '1') {
+      this.visibleItems = this.items.filter((item) => !item.isComplete);
+    } else {
+      this.visibleItems = this.items.filter((item) => item.isComplete);
     }
   }
 
